38aa8ae706975ca165228a09ab22536aca41403b
silveiratcl <silveira.tcl@gmail.com>
2026-03-20 13:27:52 -0300

    arquivos shp e mapas

--- script_guia.R
+++ script_guia.R
@@ -3,59 +3,22 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
+library(ggmap)
 library(forcats)
 
+install.packages("ggspatial")
+library(ggspatial)
+library(prettymapr)
 
-
-# Create Sample data 
-# saco_do_capim,
-# saco_do_batismo,
-# baia_das_tartarugas,
-# engenho,
-# farol  
 
 # monitoring
-df_monit = read_delim("data/dados_monitoramento_cs_2025-04-30.csv",
-                      col_types = list(localidade = col_character(),
-                                       data = col_date(format = "%d/%m/%Y"),
-                                       visib_horiz = col_double(),
-                                       faixa_bat = col_character(),
-                                       prof_min = col_double(),
-                                       prof_max = col_double(),
-                                       metodo = col_character(),
-                                       observer = col_character(),
-                                       n_divers = col_double(),
-                                       tempo_censo = col_double(),
-                                       dafor = col_double(),
-                                       iar_medio = col_double(),
-                                       n_trans_vis = col_double(),
-                                       n_trans_pres = col_double(),
-                                       dafor_id = col_double(),
-                                       geo_id = col_character(),
-                                       obs = col_character(), 
-                                       id_horus = col_double()
-                      ))
+df_guia <- read_delim("dados/data_guia.csv")
 
+df_manejo <-  read_delim("dados/dados_manejo_guia.csv")
 
+df_guia
 
-
-
-df_guia <- df_monit |> 
-  filter(localidade %in% c("saco_do_capim",
-                           "saco_do_batismo",
-                           "baia_das_tartarugas",
-                           "engenho",
-                           "farol",
-                           "vidal"), 
-         obs != "estimado dos dados do ICMBio")
-
-
-
-table(df_guia$localidade)
-
-write_csv(df_guia, "guia/data_guia.csv")
-
-
+df_manejo
 
 # Creating a same effort data set based on the original data with unnequal effort
 # keep just data with at least 30 minutes of monitoring (rows) and then sample 
@@ -221,9 +184,6 @@ ggsave("plots/stacked_dafor_localidade_balanced.png",
 
 
 
-
-
-
 ### UMBALANCED DATA ###
 
 ##############################
@@ -337,4 +297,221 @@ ggsave("plots/stacked_dafor_localidade.png",
 
 
 
+
+################################################################################
+### Dafor DATA and management  - map
+################################################################################
+
+library(sf)
+library(janitor)
+library(scatterpie)
+library(ggnewscale)
+library(ggmap)
+library(scales)
+library(lubridate)
+
+################################################################################
+### 1. Clean and prepare management data
+################################################################################
+
+df_manejo_map <- df_manejo %>%
+  clean_names() %>%
+  mutate(
+    localidade = str_to_lower(localidade),
+    localidade = str_replace_all(localidade, " ", "_"),
+    data = dmy(data),
+    massa_kg = parse_number(massa_kg, locale = locale(decimal_mark = ","))
+  )
+
+# check
+glimpse(df_manejo_map)
+
+################################################################################
+### 2. Prepare DAFOR data from balanced monitoring dataset
+################################################################################
+
+df_guia_map <- df_guia_balanced %>%
+  mutate(
+    localidade = str_to_lower(localidade),
+    localidade = str_replace_all(localidade, " ", "_"),
+    dafor_cat = case_when(
+      dafor == 10 ~ "D",
+      dafor == 8  ~ "A",
+      dafor == 6  ~ "F",
+      dafor == 4  ~ "O",
+      dafor == 2  ~ "R",
+      dafor == 0  ~ "Ausente",
+      TRUE        ~ NA_character_
+    )
+  ) %>%
+  filter(!is.na(dafor_cat))
+
+################################################################################
+### 3. Summaries
+################################################################################
+
+### 3.1 Total mass managed by locality
+massa_loc <- df_manejo_map %>%
+  group_by(localidade) %>%
+  summarise(
+    massa_total_kg = sum(massa_kg, na.rm = TRUE),
+    n_manejos = n(),
+    .groups = "drop"
+  )
+
+massa_loc
+
+### 3.2 DAFOR proportions by locality
+dafor_loc <- df_guia_map %>%
+  count(localidade, dafor_cat, name = "n") %>%
+  group_by(localidade) %>%
+  mutate(prop = n / sum(n)) %>%
+  ungroup()
+
+dafor_pies <- dafor_loc %>%
+  select(localidade, dafor_cat, prop) %>%
+  pivot_wider(
+    names_from = dafor_cat,
+    values_from = prop,
+    values_fill = 0
+  )
+
+dafor_pies
+
+################################################################################
+### 4. Read shapefiles
+################################################################################
+
+linhas_loc <- st_read("shp/localidades_guia.shp", quiet = TRUE) %>%
+  clean_names()
+
+centros_loc <- st_read("shp/centro_localidades_guia.shp", quiet = TRUE) %>%
+  clean_names()
+
+names(linhas_loc)
+names(centros_loc)
+
+################################################################################
+### 5. Standardize locality field in shapefiles
+################################################################################
+# IMPORTANT:
+# If the field name is not "localidade", replace below with the correct field.
+
+linhas_loc <- linhas_loc %>%
+  mutate(
+    localidade = str_to_lower(localidade),
+    localidade = str_replace_all(localidade, " ", "_")
+  )
+
+centros_loc <- centros_loc %>%
+  mutate(
+    localidade = str_to_lower(localidade),
+    localidade = str_replace_all(localidade, " ", "_")
+  )
+
+################################################################################
+### 6. Reproject to WGS84 for Google map
+################################################################################
+
+linhas_loc  <- st_transform(linhas_loc, 4326)
+centros_loc <- st_transform(centros_loc, 4326)
+
+################################################################################
+### 7. Join summaries to spatial layers
+################################################################################
+
+linhas_map <- linhas_loc %>%
+  left_join(massa_loc, by = "localidade") %>%
+  mutate(
+    massa_total_kg = replace_na(massa_total_kg, 0)
+  )
+
+centros_map <- centros_loc %>%
+  left_join(massa_loc, by = "localidade") %>%
+  left_join(dafor_pies, by = "localidade") %>%
+  mutate(
+    massa_total_kg = replace_na(massa_total_kg, 0),
+    D = replace_na(D, 0),
+    A = replace_na(A, 0),
+    F = replace_na(F, 0),
+    O = replace_na(O, 0),
+    R = replace_na(R, 0),
+    Ausente = replace_na(Ausente, 0)
+  )
+
+################################################################################
+### 8. Extract XY coordinates for pie charts
+################################################################################
+
+centros_xy <- centros_map %>%
+  cbind(st_coordinates(.)) %>%
+  st_drop_geometry() %>%
+  rename(x = X, y = Y)
+
+centros_xy
+
+
+
+################################################################################
+### Final map (NO Google / Simple tutorial version)
+################################################################################
+
+################################################################################
+### Final map (simple version, no basemap)
+################################################################################
+
+# guarantee same CRS
+centros_loc <- st_transform(centros_loc, st_crs(linhas_loc))
+
+# create one bbox using both layers
+bbox_all <- st_bbox(
+  st_union(
+    st_geometry(linhas_map),
+    st_geometry(centros_map)
+  )
+)
+
+map_dafor_manejo <- ggplot() +
+  geom_sf(
+    data = linhas_map,
+    aes(color = massa_total_kg),
+    linewidth = 2.2,
+    fill = NA
+  ) +
+  scale_color_viridis_c(
+    option = "magma",
+    trans = "sqrt",
+    name = "Massa manejada (kg)"
+  ) +
+  ggnewscale::new_scale_fill() +
+  scatterpie::geom_scatterpie(
+    data = centros_xy,
+    aes(x = x, y = y),
+    cols = c("D", "A", "F", "O", "R", "Ausente"),
+    color = "black",
+    alpha = 0.95,
+    pie_scale = 4
+  ) +
+  scale_fill_viridis_d(
+    option = "plasma",
+    begin = 0.9,
+    end = 0.1,
+    name = "DAFOR"
+  ) +
+  coord_sf(
+    xlim = c(bbox_all["xmin"], bbox_all["xmax"]),
+    ylim = c(bbox_all["ymin"], bbox_all["ymax"]),
+    expand = TRUE
+  ) +
+  labs(
+    title = "Manejo de coral-sol e composição DAFOR",
+    subtitle = "Cor das linhas = massa total manejada; pizzas = composição DAFOR"
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    legend.position = "right"
+  )
+
+map_dafor_manejo
 
